746dddb94713f12ca79c29f2a98395aff87f3892
Benjamin David <ben@dukt.net>
2015-11-21 17:59:00 +0100

    Fixed config

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ return [
     /**
      * OAuth Scope
      */
-    'oauthScope' => ['manage_pages', 'read_insights', 'read_mailbox', 'read_page_mailboxes']
+    'oauthScope' => ['manage_pages', 'read_insights', 'read_mailbox', 'read_page_mailboxes'],
 
     /**
      * OAuth Params
