eb00e3aba15538f689b3b637071f6126b8fda772
Benjamin David <ben@dukt.net>
2016-01-16 21:57:52 +0100

    Disabled widget when there is no token defined

--- widgets/Facebook_StatsWidget.php
+++ widgets/Facebook_StatsWidget.php
@@ -32,45 +32,47 @@ class Facebook_StatsWidget extends BaseWidget
 
         $facebookAccountId = $pluginSettings['facebookAccountId'];
 
-        $response = craft()->facebook_api->get('/me/accounts');
-        $accounts = $response['data']['data'];
+        $token = craft()->facebook_oauth->getToken();
 
-        $facebookAccount = null;
-        $insight = [];
-        $weekTotal = 0;
-
-        foreach($accounts as $k => $account)
+        if($token)
         {
-            if($account['id'] == $facebookAccountId)
+            $response = craft()->facebook_api->get('/me/accounts');
+            $accounts = $response['data']['data'];
+
+            $facebookAccount = null;
+            $insight = [];
+            $weekTotal = 0;
+
+            foreach($accounts as $k => $account)
             {
-                $facebookAccount = $account;
-                $response = craft()->facebook_api->get('/'.$account['id'].'/insights/page_fans', array(
-                    'since' => date('Y-m-d', strtotime('-7 day')),
-                    'until' => date('Y-m-d', strtotime('+1 day')),
-                ));
+                if($account['id'] == $facebookAccountId)
+                {
+                    $facebookAccount = $account;
+                    $response = craft()->facebook_api->get('/'.$account['id'].'/insights/page_fans', array(
+                        'since' => date('Y-m-d', strtotime('-6 day')),
+                        'until' => date('Y-m-d', strtotime('+1 day')),
+                    ));
 
-                $insight = $response['data']['data'][0];
+                    $insight = $response['data']['data'][0];
 
-                $weekResponse = craft()->facebook_api->get('/'.$account['id'].'/insights/page_fans', array(
-                    'since' => date('Y-m-d', strtotime('-6 day')),
-                    'until' => date('Y-m-d', strtotime('+1 day')),
-                ));
-                $weekInsight = $weekResponse['data']['data'][0];
+                    $weekTotalStart = $insight['values'][0]['value'];
+                    $weekTotalEnd = end($insight['values'])['value'];
 
-                $weekTotalStart = $weekInsight['values'][0]['value'];
-                $weekTotalEnd = end($weekInsight['values'])['value'];
-
-                $weekTotal = $weekTotalEnd - $weekTotalStart;
+                    $weekTotal = $weekTotalEnd - $weekTotalStart;
+                }
             }
+
+            $variables['account'] = $facebookAccount;
+            $variables['insight'] = $insight;
+            $variables['weekTotal'] = $weekTotal;
+
+            craft()->templates->includeCssResource('facebook/css/stats-widget.css');
+
+            return craft()->templates->render('facebook/_components/widgets/stats/body', $variables);
         }
-
-        $variables['account'] = $facebookAccount;
-        $variables['insight'] = $insight;
-        $variables['weekInsight'] = $weekInsight;
-        $variables['weekTotal'] = $weekTotal;
-
-        craft()->templates->includeCssResource('facebook/css/stats-widget.css');
-
-        return craft()->templates->render('facebook/_components/widgets/stats/body', $variables);
+        else
+        {
+            return craft()->templates->render('facebook/_components/widgets/stats/disabled');
+        }
     }
 }
