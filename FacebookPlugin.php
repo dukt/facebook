44c6e0075c9a048fba8612a56ccf33797a3e60e4
Benjamin David <ben@dukt.net>
2016-08-09 16:18:16 +0200

    Facebook plugin now requires OAuth 2.0+

--- FacebookPlugin.php
+++ FacebookPlugin.php
@@ -17,7 +17,7 @@ class FacebookPlugin extends BasePlugin
                 'name' => "OAuth",
                 'handle' => 'oauth',
                 'url' => 'https://dukt.net/craft/oauth',
-                'version' => '1.0.0'
+                'version' => '2.0.0'
             )
         );
     }
