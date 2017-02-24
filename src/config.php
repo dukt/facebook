8de8523f6fb80c88f3862493d06b67d9ed16a2b7
Benjamin David <ben@dukt.net>
2017-02-24 16:05:02 +0100

    config.php

--- src/config.php
+++ src/config.php
@@ -3,29 +3,37 @@
 return [
 
     /**
-     * Graph API version used to request the Facebook API
+     * Graph API version used to request the Facebook API.
      */
     'apiVersion' => 'v2.8',
 
     /**
-     * Whether request to APIs should be cached or not
+     * Whether request to APIs should be cached or not.
      */
     'enableCache' => true,
 
     /**
-     * The amount of time cache should last
+     * The amount of time cache should last.
      *
      * @see http://www.php.net/manual/en/dateinterval.construct.php
      */
     'cacheDuration' => 'PT1H',
 
     /**
-     * OAuth Scope
+     * OAuth scope.
      */
     'oauthScope' => ['public_profile', 'manage_pages', 'read_insights'],
 
     /**
-     * OAuth Params
+     * OAuth provider authorization options.
      */
     'oauthAuthorizationOptions' => [],
+
+    /**
+     * OAuth provider options.
+     */
+    'oauthProviderOptions' => [
+        'clientId' => '123456789012345',
+        'clientSecret' => 'zfo8G8wef8G92fZbkHF83owy7weY2300'
+    ],
 ];
