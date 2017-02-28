bbdd2b2c4b7b27465e167abc5eb04238647c68f1
Benjamin David <ben@dukt.net>
2017-02-28 10:08:48 +0100

    Removed error handling in ReportsController

--- src/controllers/ReportsController.php
+++ src/controllers/ReportsController.php
@@ -28,39 +28,8 @@ class ReportsController extends Controller
      */
     public function actionGetInsightsReport()
     {
-        try
-        {
-            $report = Facebook::$plugin->getReports()->getInsightsReport();
+        $report = Facebook::$plugin->getReports()->getInsightsReport();
 
-            return $this->asJson($report);
-        }
-        catch(\Guzzle\Http\Exception\ClientErrorResponseException $e)
-        {
-            $errorMsg = $e->getMessage();
-
-            $response = $e->getResponse();
-            $data = json_decode($response->getBody(), true);
-
-            if(!empty($data['error']['message']))
-            {
-                $errorMsg = $data['error']['message'];
-            }
-
-            return $this->asErrorJson($errorMsg);
-        }
-        catch(\Exception $e)
-        {
-            if(method_exists($e, 'getErrors'))
-            {
-                $errors = $e->getErrors();
-
-                if(isset($errors[0]['message']))
-                {
-                    return $this->asErrorJson(Craft::t($errors[0]['message']));
-                }
-            }
-
-            return $this->asErrorJson($e->getMessage());
-        }
+        return $this->asJson($report);
     }
 }
