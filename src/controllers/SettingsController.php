55e5830d0b5e4c7dcd7bbf727a5cece25e97edd7
Benjamin David <ben@dukt.net>
2017-03-01 02:32:35 +0100

    Clean up

--- src/controllers/SettingsController.php
+++ src/controllers/SettingsController.php
@@ -66,57 +66,10 @@ class SettingsController extends Controller
     {
         $plugin = Craft::$app->getPlugins()->getPlugin('facebook');
 
-        $variables = array(
-            'provider' => false,
-            'account' => false,
-            'token' => false,
-            'error' => false
-        );
-
-        $provider = Facebook::$plugin->getOauth()->getOauthProvider();
-
-        if ($provider)
-        {
-            $token = Facebook::$plugin->getOauth()->getToken();
-
-            if ($token)
-            {
-                try
-                {
-                    $account = Facebook::$plugin->getCache()->get(['getResourceOwner', $token]);
-
-                    if(!$account)
-                    {
-                        $account = $provider->getResourceOwner($token);
-                        Facebook::$plugin->getCache()->set(['getResourceOwner', $token], $account);
-                    }
-
-                    if ($account)
-                    {
-                        $variables['account'] = $account;
-                    }
-                }
-                catch(\Exception $e)
-                {
-                    Craft::trace("Couldn't get account\r\n".$e->getMessage().'\r\n'.$e->getTraceAsString(), __METHOD__);
-
-                    if(method_exists($e, 'getResponse'))
-                    {
-                        Craft::trace("GuzzleErrorResponse\r\n".$e->getResponse(), __METHOD__);
-                    }
-
-                    $variables['error'] = $e->getMessage();
-                }
-            }
-
-            $variables['token'] = $token;
-            $variables['provider'] = $provider;
-        }
-
-        $variables['redirectUri'] = Facebook::$plugin->oauth->getRedirectUri();
-        $variables['settings'] = $plugin->getSettings();
-
-        return $this->renderTemplate('facebook/settings/oauth', $variables);
+        return $this->renderTemplate('facebook/settings/oauth', [
+            'redirectUri' => Facebook::$plugin->oauth->getRedirectUri(),
+            'settings' => $plugin->getSettings(),
+        ]);
     }
 
     public function actionSaveOauthSettings()
