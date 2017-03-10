526f6d684fa65ecd9b4c15107132499c510ec6e6
Benjamin David <ben@dukt.net>
2017-03-10 11:48:02 +0100

    Reorganized config settings

--- src/config.php
+++ src/config.php
@@ -8,16 +8,16 @@ return [
     'apiVersion' => 'v2.8',
 
     /**
-     * Whether request to APIs should be cached or not.
-     */
-    'enableCache' => true,
-
-    /**
      * The amount of time cache should last.
      *
      * @see http://www.php.net/manual/en/dateinterval.construct.php
      */
     'cacheDuration' => 'PT1H',
+
+    /**
+     * Whether request to APIs should be cached or not.
+     */
+    'enableCache' => true,
 
     /**
      * OAuth client ID.
@@ -30,12 +30,12 @@ return [
     'oauthClientSecret' => null,
 
     /**
-     * OAuth scope.
-     */
-    'oauthScope' => ['public_profile', 'manage_pages', 'read_insights'],
-
-    /**
      * OAuth provider authorization options.
      */
     'oauthAuthorizationOptions' => [],
+
+    /**
+     * OAuth scope.
+     */
+    'oauthScope' => ['public_profile', 'manage_pages', 'read_insights'],
 ];
