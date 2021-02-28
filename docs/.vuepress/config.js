590028e386ff7956cc93263be16162e9ba4e408e
Benjamin David <ben@dukt.net>
2021-02-28 14:36:32 +0100

    Use the same Google Analytics tracking code as dukt.net

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: 'Facebook Documentation',
     description: 'Facebook Documentation',
     base: '/docs/facebook/v2/',
-    ga: 'UA-1547168-34',
+    ga: 'UA-1547168-20',
     themeConfig: {
         docsRepo: 'dukt/facebook',
         docsDir: 'docs',
